1f7d314128da1d44ed380af96763ca1ed5416da4
Tyler Lindsay <tyty464646@gmail.com>
2024-02-07 00:34:55 -0500

    change port

--- src/run.py
+++ src/run.py
@@ -8,7 +8,7 @@ import pickle
 app = Flask(__name__)
 
 host = '' # empty string should specify localhost
-port = 5001
+port = 6969
 max_length = 65540
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)

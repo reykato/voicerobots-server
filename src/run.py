a43346c1ce96d3175fb9df1b15c5451298ba3121
Tyler Lindsay <tyty464646@gmail.com>
2024-02-07 01:19:04 -0500

    testing

--- src/run.py
+++ src/run.py
@@ -38,11 +38,11 @@ def gen_frames():
                     else:
                         buffer += data
 
-                frame = np.frombuffer(buffer, dtype=np.uint8)
-                frame = frame.reshape(frame.shape[0], 1)
+                # frame = np.frombuffer(buffer, dtype=np.uint8)
+                # frame = frame.reshape(frame.shape[0], 1)
 
-                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-                frame = cv2.flip(frame, 1)
+                # frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                # frame = cv2.flip(frame, 1)
 
                 if frame is not None:
                     ret, buffer_test = cv2.imencode('.jpg', frame)

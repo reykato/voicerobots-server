002c01a8aa1c576e692eb3499ba80e438e30976e
Tyler Lindsay <tyty464646@gmail.com>
2024-02-07 00:30:45 -0500

    edit run.sh to reflect directory changes

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
 
-cd /root/voicerobots-server
+cd /root/voicerobots-server/src
 source /root/voicerobots-server/.venv/bin/activate
-python3 /root/voicerobots-server/run.py
+python3 /root/voicerobots-server/src/run.py

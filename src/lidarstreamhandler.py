95e8497e0eb6d0f0bc18568be680ae7a6553a069
Tyler Lindsay <accounts@foxdev.net>
2024-03-05 00:08:31 -0500

    trying setting the frame to the image directly

--- src/lidarstreamhandler.py
+++ src/lidarstreamhandler.py
@@ -62,10 +62,9 @@ class LidarStreamHandler(StreamHandler):
     def _gen_frame(self):
         print("gen frame called")
         self.fig.canvas.draw()
-        img = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+        self.frame = np.fromstring(self.fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
         # img = img.reshape(self.fig.canvas.get_width_height()[::1] + (3,))
         # self.frame = plot.plot(img, format='jpeg')
-        self.frame = img.tobytes()
         self.frame_is_new = True
         print("generated frame")
 

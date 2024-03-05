ed1bc6b0d47dd6d73df73fbc7013e3b8a20eca7c
Tyler Lindsay <accounts@foxdev.net>
2024-03-04 19:29:10 -0500

    try receiving more bytes

--- src/lidarstreamhandler.py
+++ src/lidarstreamhandler.py
@@ -13,7 +13,7 @@ class LidarStreamHandler(StreamHandler):
     def _handle_stream(self):
         while not self.stop_event.is_set():
             try:
-                received_data = self.socket.recv(1024)
+                received_data = self.socket.recv(4096)
                 if not received_data is None:
                     decoded_data = np.frombuffer(received_data, dtype=np.float32).reshape((-1, 3))
                     for point in decoded_data:

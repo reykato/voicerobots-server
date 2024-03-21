22f291998fb506820ed9ec4a904a52e152125557
Tyler Lindsay <accounts@foxdev.net>
2024-03-21 12:01:56 -0400

    add docstring for _handle_stream and set up loop

--- src/decisionmaker.py
+++ src/decisionmaker.py
@@ -11,14 +11,14 @@ class DecisionMaker(ThreadedEvent):
         Parameters:
         - vsh (VideoStreamHandler): VideoStreamHandler object.
         - lsh (LidarStreamHandler): LidarStreamHandler object.
-        - cs (ControlStream): ControlStream object.
+        - cs (ControlStream): ControlStream object (for output).
         """
 
         self.vsh = vsh
         self.lsh = lsh
         self.cs = cs
 
-        # declare variables to hold the most recent data from video, control, and lidar streams
+        # hold the most recent frame from video, control tuple from joystick, and lidar scan
         self.target_center = None
         self.lidar_scan = None
         self.control_data = None
@@ -30,6 +30,10 @@ class DecisionMaker(ThreadedEvent):
         pass
 
     def _handle_stream(self):
-        self.target_center = self.vsh.get_center()
-        self.lidar_scan = self.lsh.get_scan()
-        # need to add a method to get the control data from the flask instance
+        """
+        Updates the target_center, lidar_scan, and control_data attributes with the most recent data.
+        """
+        while not self.stop_event.is_set():
+            self.target_center = self.vsh.get_center()
+            self.lidar_scan = self.lsh.get_scan()
+            # need to add a method to get the control data from the flask instance

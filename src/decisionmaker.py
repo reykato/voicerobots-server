d654d4bed45c00f916fd8ae3ddaece106845a011
Tyler Lindsay <tyty464646@gmail.com>
2024-03-27 13:13:47 -0400

    add testing code to decisionmaker before implementing methods

--- src/decisionmaker.py
+++ src/decisionmaker.py
@@ -4,6 +4,13 @@ from controlstream import ControlStream
 from lidarstreamhandler import LidarStreamHandler
 
 class DecisionMaker(ThreadedEvent):
+    # angle range for the lidar scan to consider as the front of the robot (in degrees)
+    LIDAR_START_ANGLE = 60
+    LIDAR_END_ANGLE   = 120
+
+    # distance of closest object for which the lidar should stop the robot (in cm)
+    LIDAR_DISTANCE_THRESHOLD = 100
+
     def __init__(self, vsh:VideoStreamHandler, lsh:LidarStreamHandler, cs:ControlStream):
         """
         Class for making decisions based on the input streams.
@@ -22,7 +29,6 @@ class DecisionMaker(ThreadedEvent):
         # hold the most recent frame from video, control tuple from joystick, and lidar scan
         self.target_center = None
         self.lidar_scan = None
-        self.control_data = (0, 0)
 
     def _before_starting(self):
         pass
@@ -35,21 +41,81 @@ class DecisionMaker(ThreadedEvent):
         Updates the target_center, lidar_scan, and control_data attributes with the most recent data.
         """
         while not self.stop_event.is_set():
+            control_data = (0, 0)
+
             self.target_center = self.vsh.get_center()
             self.lidar_scan = self.lsh.get_scan()
 
-    def _send_control(self):
+            # make decisions based on the target_center from the webcam stream
+            video_control_decision = self._make_video_decision(self.target_center)
+
+            # decide whether the robot is too close to an object based on the lidar scan
+            stop_robot = self._make_lidar_decision(self.lidar_scan)
+
+            if stop_robot:                  # if too close to an object, stop the robot
+                control_data = (0, 0)
+            else:                           # if not too close to an object, use the video control decision
+                control_data = video_control_decision
+
+            # send the control data to the ControlStream object
+            self._send_control(control_data)
+
+    def _make_video_decision(target_center:tuple) -> tuple:
+        """
+        Makes a decision based on the target_center attribute.
+
+        Parameters:
+        - target_center (tuple): Tuple (x, y) containing the x and y coordinates of the target center.
+
+        Returns:
+        - tuple: Tuple (x, y) containing the x and y joystick values.
+        """
+        print(f"Target Center: {target_center}, making video decision...")
+        return (0.5, 0.5)
+    
+    def _make_lidar_decision(self, lidar_scan:list) -> bool:
+        """
+        Makes a decision based on the lidar_scan attribute.
+
+        Parameters:
+        - lidar_scan list(tuple): Tuple containing the lidar scan data.
+
+        Returns:
+        - bool: True if the robot is too close to an object, False otherwise.
+        """
+        if len(lidar_scan) == 0:
+            print("No lidar scan data...")
+            return False
+        else:
+            print(f"Making lidar decision...")
+
+            # only consider points within the front of the robot
+            lidar_scan_narrow = [x for x in lidar_scan if x[1]
+                                  >= self.LIDAR_START_ANGLE and x[1] <= self.LIDAR_END_ANGLE]
+            
+            # check if any point is too close to the robot
+            for point in lidar_scan_narrow:
+                # if the distance of the point is less than the threshold, it is too close to the robot
+                if point[2] < self.LIDAR_DISTANCE_THRESHOLD:
+                    print(f"Object detected at {point[2]} cm, stopping robot...")
+                    return True
+                
+            # if no object is too close to the robot
+            return False
+
+
+    def _send_control(self, control_data:tuple):
         """
         Sends the control data to the ControlStream object.
         """
-        self.cs.send_control(self.control_data)
+        self.cs.send_control(control_data)
 
-    def set_control_data(self, control_data:tuple):
-        """
-        Sets the control data attribute.
+    # def set_control_data(self, control_data:tuple):
+    #     """
+    #     Sets the control data attribute.
 
-        Parameters:
-        - control_data (tuple): Tuple (x, y) containing the x and y joystick values.
-        """
+    #     Parameters:
+    #     - control_data (tuple): Tuple (x, y) containing the x and y joystick values.
+    #     """
 
-        self.control_data = control_data
+    #     self.control_data = control_data

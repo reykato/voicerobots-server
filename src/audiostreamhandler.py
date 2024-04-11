e2c66f307f2bef597c8316bbc8aee80d25865d84
gabrielmarx13 <110946716+gabrielmarx13@users.noreply.github.com>
2024-04-10 21:05:16 -0400

    Update audiostreamhandler.py

--- src/audiostreamhandler.py
+++ src/audiostreamhandler.py
@@ -20,7 +20,7 @@ class AudioStreamHandler(ThreadedEvent):
         super().__init__()
         self.host = host
         self.port = port
-        self.model = whisper.load_model("medium.en")
+        self.model = whisper.load_model("tiny.en")
         self.text = ""
 
 

7d139972d14938359cd45638110774c61f19624c
Tyler Lindsay <tyty464646@gmail.com>
2024-04-18 12:23:48 -0400

    Adjust color thresholds in VideoStreamHandler class

--- src/videostreamhandler.py
+++ src/videostreamhandler.py
@@ -174,10 +174,10 @@ class VideoStreamHandler(ThreadedEvent):
         if color == 'red':
             # self.lower = np.array([0, 120, 70])
             # self.upper = np.array([10, 255, 255])
-            self.lower = np.array([170, 100, 150])
+            self.lower = np.array([170, 100, 80])
             self.upper = np.array([179, 255, 255])
         elif color == 'blue':
-            self.lower = np.array([110, 100, 100])
+            self.lower = np.array([110, 100, 80])
             self.upper = np.array([130, 255, 255])
         elif color == 'green':
             self.lower = np.array([40, 50, 50])
